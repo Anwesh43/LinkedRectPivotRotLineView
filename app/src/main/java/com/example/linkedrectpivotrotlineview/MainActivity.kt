8ad855439f41e32c19f127d3ba5ee3caab9a5a9d
Anwesh43 <anweshthecool0@gmail.com>
2020-12-02 03:22:05 +0530

    adding fullscreen support

--- app/src/main/java/com/example/linkedrectpivotrotlineview/MainActivity.kt
+++ app/src/main/java/com/example/linkedrectpivotrotlineview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedrectpivotrotlineview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.rectpivotrotlineview.RectPivotRotLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectPivotRotLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
