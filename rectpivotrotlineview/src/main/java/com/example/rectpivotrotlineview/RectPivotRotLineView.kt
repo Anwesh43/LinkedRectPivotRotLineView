f8a8f90841068f17ee46aa3756607e156d7bb9a5
Anwesh43 <anweshthecool0@gmail.com>
2020-12-02 02:48:05 +0530

    adding constants and libraries

--- rectpivotrotlineview/src/main/java/com/example/rectpivotrotlineview/RectPivotRotLineView.kt
+++ rectpivotrotlineview/src/main/java/com/example/rectpivotrotlineview/RectPivotRotLineView.kt
@@ -1,2 +1,27 @@
 package com.example.rectpivotrotlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#9C27B0",
+    "#009688",
+    "#FF9800",
+    "#2196F3"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
