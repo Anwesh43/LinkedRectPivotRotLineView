208906736fdf85514f539f1e61b30c4a7f503d1d
Anwesh43 <anweshthecool0@gmail.com>
2020-12-02 03:07:20 +0530

    adding RectPivotRotLineView class

--- rectpivotrotlineview/src/main/java/com/example/rectpivotrotlineview/RectPivotRotLineView.kt
+++ rectpivotrotlineview/src/main/java/com/example/rectpivotrotlineview/RectPivotRotLineView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawRPRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRectPivotRotLine(scale, w, h, paint)
 }
+
+class RectPivotRotLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
